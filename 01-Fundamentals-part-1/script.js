ad8ad4bb2f2dca3135f4e5019b72c0baea887fbe
anmarkosyan <anush.markos@gmail.com>
2021-07-08 16:40:05 +0400

    added timers inside fs callback function

--- 01-Fundamentals-part-1/script.js
+++ 01-Fundamentals-part-1/script.js
@@ -9,6 +9,12 @@ process.nextTick(() => console.log('Outside next tick'));
 
 fs.readFile('test-file.txt', 'utf-8', (err, data) => {
   console.log('I/O finished');
+
+  setTimeout(() => console.log('Timer 2'), 0);
+  setTimeout(() => console.log('Timer 3'), 3000);
+  setImmediate(() => console.log('Immediate 2'));
+
+  process.nextTick(() => console.log('Inside next tick'));
 });
 
 console.log('------- End of top-level code-------');
