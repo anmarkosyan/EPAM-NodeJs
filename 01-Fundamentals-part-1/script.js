b571c7ff770888ba0b8f85ae28fe65a6b1613b22
anmarkosyan <anush.markos@gmail.com>
2021-07-08 14:32:50 +0400

    first commit

---
+++ 01-Fundamentals-part-1/script.js
@@ -0,0 +1,6 @@
+console.log('------ Start -------');
+
+setTimeout(() => console.log('Timer 1'), 0);
+setImmediate(() => console.log('Immediate 1'));
+
+process.nextTick(() => console.log('Outside next tick'));
