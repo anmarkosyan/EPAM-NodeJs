e7253d4ca572ec59d422a414ffb4c971fece3dd8
anmarkosyan <anush.markos@gmail.com>
2021-07-08 16:54:42 +0400

    added crypto module

--- 01-Fundamentals-part-1/script.js
+++ 01-Fundamentals-part-1/script.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const start = new Date();
+
 console.log('------ Start -------');
 
 setTimeout(() => console.log('Timer 1'), 0);
@@ -5,4 +10,19 @@ setImmediate(() => console.log('Immediate 1'));
 
 process.nextTick(() => console.log('Outside next tick'));
 
-console.log('------- End -------')
+fs.readFile('test-file.txt', 'utf-8', () => {
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(new Date() - start, 'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(new Date() - start, 'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(new Date() - start, 'password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(new Date() - start, 'password encrypted');
+  });
+});
+
+console.log('------- End -------');
