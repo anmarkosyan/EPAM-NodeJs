be1628d53effbb523ccbf9d96f43057b5dcebb21
anmarkosyan <anush.markos@gmail.com>
2021-07-08 16:58:06 +0400

    changed outer timer 0 to 4m

--- 01-Fundamentals-part-1/script.js
+++ 01-Fundamentals-part-1/script.js
@@ -1,6 +1,6 @@
 console.log('------ Start -------');
 
-setTimeout(() => console.log('Timer 1'), 0);
+setTimeout(() => console.log('Timer 1'), 4000);
 setImmediate(() => console.log('Immediate 1'));
 
 process.nextTick(() => console.log('Outside next tick'));
