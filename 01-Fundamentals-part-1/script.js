e169a1a4cf5f09982599ec1c1d342957ae257b9e
Anush Markosyan <anush.markos@gmail.com>
2021-07-08 14:50:19 +0400

    Update script.js
    Added console at the end.

--- 01-Fundamentals-part-1/script.js
+++ 01-Fundamentals-part-1/script.js
@@ -4,3 +4,5 @@ setTimeout(() => console.log('Timer 1'), 0);
 setImmediate(() => console.log('Immediate 1'));
 
 process.nextTick(() => console.log('Outside next tick'));
+
+console.log('------- End -------')
