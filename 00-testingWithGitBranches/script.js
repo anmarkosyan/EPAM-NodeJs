816488e0c31e040e3aee8c01c40d6a07f5b7089c
anmarkosyan <anush.markos@gmail.com>
2021-07-09 15:53:00 +0400

    using calc module

--- 00-testingWithGitBranches/script.js
+++ 00-testingWithGitBranches/script.js
@@ -1,7 +1,10 @@
 //console.log(globalThis);
+const calc = require('./calc');
 
 console.log('Hello from top-level code 🤩');
 
 setTimeout(() => console.log('First timeout'), 2000);
+
+setTimeout(calc, 1000, 3, 5);
 
 console.log('Bye from top-level code 😎');
