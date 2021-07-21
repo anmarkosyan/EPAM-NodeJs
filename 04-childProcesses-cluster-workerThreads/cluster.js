b7bb6a427f3c9590b59bcb6445712d63965936a1
anmarkosyan <anush.markos@gmail.com>
2021-07-21 09:50:44 +0400

    added exit event

--- 04-childProcesses-cluster-workerThreads/cluster.js
+++ 04-childProcesses-cluster-workerThreads/cluster.js
@@ -58,7 +58,10 @@ if (cluster.isMaster) {
       console.log(msg);
     });
   });
-} else if (cluster.isWorker) {
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
   http
     .createServer((req, res) => {
       res.writeHead(200);
