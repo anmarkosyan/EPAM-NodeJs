06b43b9b4d57fcc8b4023c80ac44967261bb858c
anmarkosyan <anush.markos@gmail.com>
2021-07-21 09:41:51 +0400

    connected parent to child with IPC

--- 04-childProcesses-cluster-workerThreads/cluster.js
+++ 04-childProcesses-cluster-workerThreads/cluster.js
@@ -27,25 +27,45 @@ Write a function which will
 2.and return the main result to main thread and sum up in main thread and print the result on the console.
 3.Make sure that you will fork process for 2 processor.
  */
+// const forks = 2;
+// const factorial = n => (n ? factorial(n - 1) * n : 1);
+// if (cluster.isMaster) {
+//   console.log('hi master');
+//   for (let i = 0; i < forks; i++) {
+//     const worker = cluster.fork();
+//     //console.log('masters workers')
+//     worker.send(factorial(50));
+//   }
+// } else if (cluster.isWorker) {
+//   process.on('message', msg => {
+//     console.log(msg);
+//   });
+//   //console.log('workers')
+// }
+
 const forks = 2;
 const factorial = n => (n ? factorial(n - 1) * n : 1);
 
 if (cluster.isMaster) {
   console.log(`[${process.pid}] Forking for 2 CPUs`);
+
   for (let i = 0; i < forks; i++) {
     cluster.fork();
   }
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
+
+  Object.values(cluster.workers).forEach(el => {
+    el.on('message', msg => {
+      console.log(msg);
+    });
   });
-} else {
+} else if (cluster.isWorker) {
   http
     .createServer((req, res) => {
       res.writeHead(200);
-      res.end('hello world\n');
+      res.end();
     })
     .listen(8000);
 
-  console.log(`[${process.pid}] ${factorial(50)} ${cluster.worker.id}`);
+  process.send(`[${process.pid}] ${factorial(50)} ${cluster.worker.id}`);
   process.exit(0);
 }
