afa43855e127d0c5d9732a6618a1f19797fcfb59
anmarkosyan <anush.markos@gmail.com>
2021-07-27 12:26:00 +0400

    some about buffers

--- 06-Buffer-Stream/buffer.ts
+++ 06-Buffer-Stream/buffer.ts
@@ -1,2 +1,9 @@
+//==================== 🔴 lecture part ============
+//Remember how we had to import the Console class in the previous lesson? Unlike the Console class,
+//the Buffer class is in the global definition,so we do not need to import anything.
+
 const buf1 = Buffer.alloc(10);
 console.log(buf1);
+
+const strBuf = Buffer.from('hello there!!');
+console.log(strBuf.toString());
