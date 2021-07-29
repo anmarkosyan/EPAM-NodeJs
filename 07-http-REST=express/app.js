a4778acabcd7736c68fa3c2fda6547b85d898b9a
anmarkosyan <anush.markos@gmail.com>
2021-07-29 23:15:52 +0400

    get and post routing

--- 07-http-REST=express/app.js
+++ 07-http-REST=express/app.js
@@ -4,24 +4,41 @@ import express from 'express';
 
 const app = express();
 const __dirname = path.resolve();
-
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/courses.json`, 'utf-8'));
+
+//middlewares
+app.use(express.json());
+
 //1️⃣ HTTP get method
 app.get('/api/v1/courses', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: courses.length,
     data: {
-       courses,
+      courses,
     },
   });
 });
 
 //2️⃣ HTTP post method
-app.post('/api/v1/courses', (req, res)=>{
+app.post('/api/v1/courses', (req, res) => {
+  //console.log(req.body);
+  const newId = courses[courses.length - 1].id + 1;
+  const newCourse = Object.assign({ id: newId }, req.body);
 
-})
+  courses.push(newCourse);
+  console.log(courses);
 
+  fs.writeFile(`${__dirname}/dev-data/data/courses.json`, JSON.stringify(courses), err => {
+    if (err) throw err;
+    res.status(201).json({
+      status: 'success',
+      data: {
+        courses: newCourse,
+      },
+    });
+  });
+});
 
 //creating server
 const port = 8000;
