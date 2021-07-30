bd25eff32188ba737d8454192908edddccda1682
anmarkosyan <anush.markos@gmail.com>
2021-07-30 15:26:55 +0400

    handling patch requests

--- 07-http-REST=express/app.js
+++ 07-http-REST=express/app.js
@@ -20,6 +20,26 @@ app.get('/api/v1/courses', (req, res) => {
   });
 });
 
+app.get('/api/v1/courses/:id', (req, res) => {
+  //console.log(req.params);
+  const id = Number(req.params.id); // req.params.id * 1;
+  const course = courses.find(el => el.id === id);
+
+  if (!course) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid ID',
+    });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        course,
+      },
+    });
+  }
+});
+
 //2️⃣ HTTP post method
 app.post('/api/v1/courses', (req, res) => {
   //console.log(req.body);
@@ -27,7 +47,7 @@ app.post('/api/v1/courses', (req, res) => {
   const newCourse = Object.assign({ id: newId }, req.body);
 
   courses.push(newCourse);
-  console.log(courses);
+  //console.log(courses);
 
   fs.writeFile(`${__dirname}/dev-data/data/courses.json`, JSON.stringify(courses), err => {
     if (err) throw err;
@@ -39,6 +59,40 @@ app.post('/api/v1/courses', (req, res) => {
     });
   });
 });
+
+//3️⃣ HTTP patch method
+app.patch('/api/v1/courses/:id', (req, res) => {
+  //console.log(req.params, req.body);
+  const id = Number(req.params.id);
+  const course = courses.find(el => el.id === id);
+  const updateCourse = Object.assign(course, req.body);
+  //console.log(updateCourse);
+
+  if (!course) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid ID',
+    });
+  } else {
+    for (let el of courses) {
+      if (el.id === updateCourse.id) {
+        el = updateCourse;
+      }
+    }
+
+    fs.writeFile(`${__dirname}/dev-data/data/courses.json`, JSON.stringify(courses), err => {
+      if (err) throw err;
+      res.status(201).json({
+        status: 'success',
+        data: {
+          updateCourse,
+        },
+      });
+    });
+  }
+});
+
+app.delete('/api/v1/courses/:id', (req, res) => {});
 
 //creating server
 const port = 8000;
