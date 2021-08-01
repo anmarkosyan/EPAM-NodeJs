501516941483184924d9f6c1230200b1d585363f
anmarkosyan <anush.markos@gmail.com>
2021-08-01 20:33:04 +0400

    how to work with static files

--- 07-http-Express-Rest/app.js
+++ 07-http-Express-Rest/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
 import courseRouter from './routes/courseRoutes.js';
-import userRoutes from './routes/userRoutes';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static('./public'));
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -15,6 +17,6 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.use('/api/v1/courses', courseRouter);
-app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/users', userRoutes);
 
 export default app;
