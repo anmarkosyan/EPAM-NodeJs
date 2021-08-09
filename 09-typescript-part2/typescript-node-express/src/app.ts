1d59bff7f55179e6a073bcb797e9aeb612e114b4
anmarkosyan <anush.markos@gmail.com>
2021-08-09 11:19:16 +0400

    added express simple get route

--- 09-typescript-part2/typescript-node-express/src/app.ts
+++ 09-typescript-part2/typescript-node-express/src/app.ts
@@ -1,2 +1,14 @@
-const greeting: string = '';
-const numbers: number[] = [2, 3, 4, 5];
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+
+const add = (a: number, b: number): number => a + b;
+app.get('/', (req: Request, res: Response) => {
+  console.log(add(3, 5));
+  res.send('Nice to meet you typescript 😍');
+});
+
+const port: number = 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
