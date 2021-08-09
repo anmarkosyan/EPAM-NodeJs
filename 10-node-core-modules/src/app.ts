98113b8bd9f742eebfecdf560c27580fcd579524
anmarkosyan <anush.markos@gmail.com>
2021-08-09 22:27:51 +0400

    added new packages for more advande work with req/res

--- 10-node-core-modules/src/app.ts
+++ 10-node-core-modules/src/app.ts
@@ -1,12 +1,21 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import compression from 'compression';
+
 
 dotenv.config();
 export const PORT = process.env.PORT || 4000;
 
-
-export const getApp = ():Express => {
+export const getApp = (): Express => {
   const app = express();
+  app.disable('x-powered-by');
+  app.use(compression());
+
+  app.use(cors());
+  app.get('/ping', (req: Request, res: Response) => {
+    res.send('hello there))))');
+  });
 
   return app;
-}
+};
