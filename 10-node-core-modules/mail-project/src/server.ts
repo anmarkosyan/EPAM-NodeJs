8fe0ea2942e817562563d12243c86514f2e2f01e
anmarkosyan <anush.markos@gmail.com>
2021-08-15 18:41:53 +0400

    handling unhandled promise rejactions

--- 10-node-core-modules/mail-project/src/server.ts
+++ 10-node-core-modules/mail-project/src/server.ts
@@ -11,8 +11,10 @@ const server = () => {
 };
 
 //central place to handle all UNHANDLED PROMISE REJECTIONS,catch  form anywhere in the application
-// process.on('unhandledRejection', err:{} => {
+// process.on('unhandledRejection', err => {
 //   console.log(err.name, err.message);
-//   process.exit(1);
-// })
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
 server();
