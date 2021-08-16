4b5918fbb3e1af2a4d43df60f81b1e0bdd71ce99
anmarkosyan <anush.markos@gmail.com>
2021-08-16 10:04:52 +0400

    DON’T USE gzip compression

--- 10-node-core-modules/mail-project/src/app.ts
+++ 10-node-core-modules/mail-project/src/app.ts
@@ -1,13 +1,18 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import emailRoutes from '../router/emailsRoutes';
 import { AppError } from '../utils/appError';
 import { globalErrorHandler } from '../controller/errorController';
-import compression from 'compression';
+//❗️ Compressing a response involves CPU computation.
+//So it’s better to delegate that kind of tasks to the proxy,
+// CloudFront has the option to set compression,
+// as well as other proxies like Nginx.
+
+//import compression from 'compression';
+//app.use(compression());
 
 const app = express();
 app.use(express.json());
-app.use(compression());
 app.use(cors({}));
 
 app.use('/api/v1/', emailRoutes);
