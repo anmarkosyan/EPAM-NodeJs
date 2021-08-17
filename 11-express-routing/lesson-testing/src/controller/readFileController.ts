0a96d80c834cddc5d6db24ef6ee37a7d0ce01756
anmarkosyan <anush.markos@gmail.com>
2021-08-17 20:47:07 +0400

    added another way of reading big file, and send that file to the user

--- 11-express-routing/lesson-testing/src/controller/readFileController.ts
+++ 11-express-routing/lesson-testing/src/controller/readFileController.ts
@@ -2,15 +2,17 @@ import fs from 'fs';
 import path from 'path';
 import { Request, Response } from 'express';
 
-
 export const readFile = (req: Request, res: Response) => {
   const __dirname = path.resolve();
   const filePath = `${__dirname}/src/data/bigData.txt`;
+   //fs.createReadStream(filePath).pipe(res); //=> in this case user will see plain text file
 
-   fs.createReadStream(filePath).pipe(
+  //there is good if  send json formatted file to the client
+  fs.createReadStream(filePath).pipe(
     res.status(200).json({
       status: 'success',
       message: 'Reading text is done 🎉',
     })
   );
+
 };
