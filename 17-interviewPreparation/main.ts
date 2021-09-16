a1430d1845035123f34859ac53e5cafd7f8d34d2
anmarkosyan <anush.markos@gmail.com>
2021-09-16 13:30:42 +0400

    added some new tasks

--- 17-interviewPreparation/main.ts
+++ 17-interviewPreparation/main.ts
@@ -49,18 +49,68 @@ const tuple: {
 } = {
   role: [2, 'admin'],
 };
-tuple.role.push(45);//!!!but dont do this, ony that count of values which is specifies
+tuple.role.push(45); //!!!but dont do this, ony that count of values which is specifies
 tuple.role.push('user');
 
 //console.log(tuple.role);
 
 //enum type enum{NEW = 2, NEW1 = 10, NEW2 = 15} => object with identifier values
 //also for mapping values
-enum Role{'ADMIN', 'USER','EDITOR'}
-
-const enums = {
-  role: Role.ADMIN //=> doing mapping value
+enum Role {
+  'ADMIN' = 3,
+  'USER',
+  'EDITOR',
 }
 
-console.log(enums.role);
+const enums = {
+  role: Role.ADMIN, //=> doing mapping value
+};
 
+//console.log(enums.role);
+
+//any type => avoid using this typescript data type!!!!
+
+//union type, aliases
+type Combinable = number | string; // => creating alias for our types
+
+function combine(a: Combinable, b: Combinable): Combinable {
+  let result;
+  if (typeof a === 'number' && typeof b === 'number') {
+    result = a + b;
+  } else {
+    return a.toString() + b.toString();
+  }
+  return result;
+}
+const combineAge = combine(3, 5);
+const combineNames = combine('Anush', 'Markos');
+// console.log(combineAge);
+// console.log(combineNames);
+
+//functions
+function add2(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add2(5, 7));
+
+//interface
+// class Point {
+//   constructor(private x?: number, private y?: number) {}
+//
+//   add() {
+//     const res = this.x ;
+//     console.log('hi ' + res);
+//   }
+// }
+//
+// const point = new Point(3, 5);
+// point.add();
+
+for (var i = 0; i < 5; i++) {
+  setTimeout((y)=>{
+    console.log(y)
+  }, 2000, i)
+  //console.log(i);
+}
+//console.log(i)
