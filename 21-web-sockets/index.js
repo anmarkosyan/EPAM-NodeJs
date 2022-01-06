92f6cb04ade76d4bd015495e5f530f897d861d35
Anush Markosyan <Anush_Markosyan@epam.com>
2022-01-06 11:20:41 +0400

    completed first  message room

---
+++ 21-web-sockets/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const port = 8000;
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
+
+app.use(express.static('public'));
+
+const io = socket(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+//tech namespace
+const tech = io.of('/tech');
+tech.on('connection', socket => {
+  socket.on('join', data => {
+    socket.join(data.room);
+    tech.in(data.room).emit('message', `New user joined ${data.room} room!!`);
+  });
+
+  socket.on('message', data => {
+    tech.in(data.room).emit('message', data.msg);
+  });
+
+  socket.on('disconnect', () => {
+    tech.emit('message', 'user disconnected');
+  });
+});
