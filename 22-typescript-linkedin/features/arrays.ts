43178e67320d3e905bab0e69b4b67826f299e95c
Anush Markosyan <Anush_Markosyan@epam.com>
2022-02-09 15:35:15 +0400

    added arrays and intefaces

---
+++ 22-typescript-linkedin/features/arrays.ts
@@ -0,0 +1,7 @@
+const cars: string[] = [];
+const carsName = ['toyota', 'mini cooper', 'ford', 34];
+const mixedArr: (string | number | Date)[] = [];
+mixedArr.push('hello');
+mixedArr.push(1234);
+mixedArr.push(new Date());
+console.log(mixedArr);
